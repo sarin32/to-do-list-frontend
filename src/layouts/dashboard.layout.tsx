e2a64409fff6efaf17e194695b6863db9b54ec3b
sarin32 <execode02@gmail.com>
2024-05-25 09:10:36 +0530

    [fix] sidebar opening issue

--- src/layouts/dashboard.layout.tsx
+++ src/layouts/dashboard.layout.tsx
@@ -16,11 +16,15 @@ export const DashboardLayout = () => {
     setIsSidebarOpen(!isSidebarOpen);
   };
 
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
   return (
     <div className="flex min-h-screen">
       {/* Sidebar/Navigation */}
       <aside
-        className={`bg-secondary text-primary-foreground w-64 fixed md:relative inset-y-0 transform ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0 transition-transform duration-300 ease-in-out md:w-64 lg:w-64`}
+        className={`bg-secondary text-primary-foreground w-64 fixed md:relative inset-y-0 transform ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0 transition-transform duration-300 ease-in-out md:w-64 lg:w-64 z-10`}
       >
         <div className="py-4 px-6">
           <Logo />
@@ -32,6 +36,7 @@ export const DashboardLayout = () => {
                   <Link
                     className="block text-secondary-foreground hover:text-primary"
                     to={route.path}
+                    onClick={closeSidebar}
                   >
                     {route.title}
                   </Link>
