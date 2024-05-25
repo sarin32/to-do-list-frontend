69eff6b3abd0c0831a3b1df0ca85c0e9adaf3495
sarin32 <execode02@gmail.com>
2024-05-25 08:22:55 +0530

    [fix] prod image loading issue fix

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,9 +1,10 @@
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import logo from '../assets/images/logo.svg';  // Adjust the path as necessary
 
 export function Logo() {
   return (
     <div className="flex items-center space-x-2">
-      <img src="/src/assets/images/logo.svg" className="h-8" alt="Logo" />
+      <img src={logo} className="h-8" alt="Logo" />
       <h1 className="text-2xl font-bold text-secondary-foreground">
         <Link to={'/'}>ClearList</Link>
       </h1>
